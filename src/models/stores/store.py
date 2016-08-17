928844ca68b3a08f668effdea07546f22ea9e5f7
leverage2014 <myletter428@gmail.com>
2016-08-16 22:43:02 -0400

    Add store errors handling

--- src/models/stores/store.py
+++ src/models/stores/store.py
@@ -2,6 +2,7 @@ import uuid
 
 from src.common.database import Database
 import src.models.stores.constants as StoreConstants
+import src.models.stores.errors as StoreErrors
 
 class Store(object):
     def __init__(self, name, url_prefix, tag_name, query, _id=None):
@@ -36,8 +37,15 @@ class Store(object):
 
     @classmethod
     def get_by_url_prefix(cls, url_prefix):
-        data = Database.find_one(StoreConstants.COLLECTION, {"url_prefix": {"$regex":"^{}".format(url_prefix)}})
-        if data is not None:
-            return cls(**data)
-        else:
-            return None
+        return cls(**Database.find_one(StoreConstants.COLLECTION, {"url_prefix": {"$regex":"^{}".format(url_prefix)}}))
+
+
+    @classmethod
+    def find_by_url(cls, url):
+        for i in range(0, len(url)+1):
+            try:
+                store = cls.get_by_url_prefix(url[:i])
+                return store
+            except:
+                raise StoreErrors.StoreNotFoundException("The URL prefix used to find the store didn't give us any results")
+
